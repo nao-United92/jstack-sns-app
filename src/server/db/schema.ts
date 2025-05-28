3dacf72dea5aa87806d98ab7328d60e0f4f3f667
nao-United92 <naoyauver@icloud.com>
2025-05-29 01:28:20 +0900

    add users schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,28 @@
-import { pgTable, serial, text, timestamp, index } from "drizzle-orm/pg-core"
+import { pgTable, serial, text, timestamp, index } from 'drizzle-orm/pg-core';
 
 export const posts = pgTable(
-  "posts",
+  'posts',
   {
-    id: serial("id").primaryKey(),
-    name: text("name").notNull(),
-    createdAt: timestamp("createdAt").defaultNow().notNull(),
-    updatedAt: timestamp("updatedAt").defaultNow().notNull(),
+    id: serial('id').primaryKey(),
+    name: text('name').notNull(),
+    createdAt: timestamp('createdAt').defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt').defaultNow().notNull(),
   },
-  (table) => [
-    index("Post_name_idx").on(table.name)
-  ]
-)
+  (table) => [index('Post_name_idx').on(table.name)]
+);
+
+export const users = pgTable(
+  'users',
+  {
+    id: serial('id').primaryKey(),
+    clerkId: text('clerkId').notNull(),
+    email: text('email').notNull(),
+    name: text('name').notNull(),
+    handle: text('handle').notNull(),
+    avatarUrl: text('avatarUrl').notNull(),
+    bio: text('bio').notNull(),
+    createdAt: timestamp('createdAt').defaultNow().notNull(),
+    updatedAt: timestamp('updatedAt').defaultNow().notNull(),
+  },
+  (table) => [index('User_email_idx').on(table.email)]
+);
