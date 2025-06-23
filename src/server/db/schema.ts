653eb7030dbc59b80390d7b5eea69bbc1f22dd0a
nao-United92 <naoyauver@icloud.com>
2025-06-24 01:05:22 +0900

    fix posts schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,14 +1,24 @@
-import { pgTable, serial, text, timestamp, index } from 'drizzle-orm/pg-core';
+import {
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  index,
+  integer,
+} from 'drizzle-orm/pg-core';
 
 export const posts = pgTable(
   'posts',
   {
     id: serial('id').primaryKey(),
-    name: text('name').notNull(),
+    content: text('content').notNull(),
+    handle: text('handle').notNull(),
+    like: integer('like').notNull(),
+    image: text('image'),
     createdAt: timestamp('createdAt').defaultNow().notNull(),
     updatedAt: timestamp('updatedAt').defaultNow().notNull(),
   },
-  (table) => [index('Post_name_idx').on(table.name)]
+  (table) => [index('Post_handle_idx').on(table.handle)]
 );
 
 export const users = pgTable(
