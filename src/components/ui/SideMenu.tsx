b7a58b20dc64f869dc00490af78e2b4d4943f510
nao-United92 <naoyauver@icloud.com>
2025-06-25 23:23:11 +0900

    create SideMenu

--- src/components/ui/SideMenu.tsx
+++ src/components/ui/SideMenu.tsx
@@ -1,10 +1,23 @@
 import { SignOutButton } from '@clerk/nextjs';
 import React from 'react';
+import { Search, ListTodo, Bookmark, User, Twitter } from 'lucide-react';
+import Link from 'next/link';
+
+const menuItems = [
+  { name: 'Explore', icon: Search, href: '/' },
+  { name: 'Lists', icon: ListTodo, href: '/profile' },
+  { name: 'Bookmarks', icon: Bookmark, href: '/bookmarks' },
+  { name: 'Profile', icon: User, href: '/profile' },
+];
 
 function SideMenu() {
   return (
-    <div>
-      <SignOutButton />
+    <div className="fixed top-0 left-0 h-screen flex flex-col border-r w-64 py-4 px-2 bg-white z-30 overflow-y-auto shadow-md">
+      <div className="px-4 md-6">
+        <Link href="/" className="flex items-center gap-2">
+          <Twitter className="h-8 w-8 text-blue-500" />
+        </Link>
+      </div>
     </div>
   );
 }
